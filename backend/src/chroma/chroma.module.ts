313ad0e07a39dcf44124609b015f45b31ca63e8f
Yong Jia Jie <jiajie.yong@cognizant.com>
2025-06-03 15:48:49 +0800

    feat: import filehandler service

--- backend/src/chroma/chroma.module.ts
+++ backend/src/chroma/chroma.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EmbeddingService } from 'src/embedding/embedding.service';
+import { FileHandlerService } from 'src/fileHandler/fileHandler.service';
 import { ChromaController } from './chroma.controller';
 import { ChromaService } from './chroma.service';
 
+//TO-DO: handle the providers into their own modules
 @Module({
-  providers: [ChromaService, EmbeddingService],
+  providers: [ChromaService, EmbeddingService, FileHandlerService],
   controllers: [ChromaController],
 })
 export class ChromaModule {}
